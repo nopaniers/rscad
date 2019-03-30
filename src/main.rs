4a2e4e3d9e2d0df93d284d3219e2672db5642e46
Charles Hill <cdhill@unimelb.edu.au>
2019-03-30 12:57:00 +1100

    Now with nice spheres

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,9 @@ use std::ops::{Add, Sub, Neg, Div, AddAssign, MulAssign};
 use std::f32::consts::{PI};
 use std::fs::File;
 
+#[macro_use]
 extern crate itertools;
-use itertools::zip;
+use itertools::{zip, interleave};
 
 extern crate byteorder;
 use std::io::{Result, Write};
@@ -80,7 +81,7 @@ impl Point {
       (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
    }
 
-   fn unit_vector(self) -> Vector {
+   fn normalize(self) -> Vector {
       self/self.length()
    }
 }
@@ -176,6 +177,13 @@ impl Triangle {
    fn new(p1: Point, p2: Point, p3: Point) -> Triangle {
       Triangle{ vertex: [p1, p2, p3] }
    }
+
+   fn invert(&mut self) {
+      // Swaps points 1 and 2 so that the normal points the other way
+      let (p1, p2) = (self.vertex[1], self.vertex[2]);
+      self.vertex[1] = p2;
+      self.vertex[2] = p1;
+   }
 }
 
 
@@ -200,6 +208,7 @@ impl AddAssign<Vector> for Triangle {
       }
    }
 }
+
 
 impl MulAssign<Length> for Triangle {
    fn mul_assign(&mut self, scale: Length) {
@@ -236,6 +245,14 @@ struct Face {
 }
 
 
+impl Face {
+
+   fn invert(&mut self) {
+      self.triangle.invert();
+   }
+}
+
+
 #[derive(Clone, Copy)]
 struct Colour {
    r: u8,
@@ -244,27 +261,36 @@ struct Colour {
    alpha: u8
 }
 
+
 #[derive(Clone)]
 struct Object {
    faces: Vec<Face>
 }
 
 
-
-
 impl Object {
 
+   fn inverted(&self) -> Object {
+      let mut obj = self.clone();
+      for face in &mut obj.faces {
+         face.invert();
+      }
+      obj
+   }
+   
    //---------------------------------------------------------------------------
    // Basic Objects
    //---------------------------------------------------------------------------
-
+ 
    fn umbrella(centre: &Point, spokes: Vec<Point>) -> Object {
       let mut obj = Object::new();
       
-      for (p2, p3) in zip(&vertices, &vertices[1..]) {
-         obj += Triangle::new(centre, *p2, *p3);
+      for (p2, p3) in zip(&spokes, &spokes[1..]) {
+         obj += Triangle::new(*centre, *p3, *p2);
       }
-      obj += Triangle::new(centre, vertices[0], vertices[-1]);
+      obj += Triangle::new(*centre,
+                           *spokes.first().unwrap(),
+                           *spokes.last().unwrap());
       
       obj
    }
@@ -280,27 +306,84 @@ impl Object {
       // vertices are at evenly spaced longitudes (36° apart),
       // alternating between north and south latitudes.
 
-      let obj = Object::new();
-      use Object::umbrella;
+      let mut obj = Object::new();
+      // use Object::umbrella;
       
       let lat = 0.5_f32.atan();
-      let theta = PI/5.0;
+      let c = (1.0 - lat*lat).sqrt();
+      let theta = 2.0*PI/5.0;
 
-      obj += umbrella(Point{x: 0.0, y: 0.0, z: 1.0},
-                      (0..5).map(|i| Point{x: (theta*i as f32).cos(),
-                                           y: (theta*i as f32).sin(),
-                                           z: lat}).collect::Vec<Point>());
+      // Construct the top
+      let top_points = (0..5)
+         .map(|i| Point{x: c*(theta*i as f32).cos(),
+                        y: c*(theta*i as f32).sin(),
+                        z: lat})
+         .collect::<Vec<Point>>();
       
+      obj += Object::umbrella(&Point{x: 0.0, y: 0.0, z: 1.0},
+                              top_points.clone()).inverted();
 
+
+      // Construct the bottom
+      let bottom_points = (0..5)
+         .map(|i| Point{x: c*(theta*(0.5+i as f32)).cos(),
+                        y: c*(theta*(0.5+i as f32)).sin(),
+                        z: -lat})
+         .collect::<Vec<Point>>();
+
+      obj += Object::umbrella(&Point{x: 0.0, y: 0.0, z: -1.0},
+                              bottom_points.clone());
+
+      // Construct the strip joining top and bottom
+      for (top, top_next, bottom, bottom_next)
+         in izip!(&top_points, &top_points[1..],
+                  &bottom_points, &bottom_points[1..]) {
+            obj += Triangle::new(*bottom, *top_next, *top);
+            obj += Triangle::new(*bottom, *bottom_next, *top_next);
+      }
+      obj += Triangle::new(top_points[0], top_points[4], bottom_points[4]);
+      obj += Triangle::new(bottom_points[4], bottom_points[0], top_points[0]);
+      
+      obj.scale(radius);
       obj
    }
-   
-   fn sphere(radius: Length) {
 
+   
+   fn spherify(obj: Object) -> Object {
+      let mut sphere = Object::new();
+
+      for face in obj.faces {
+         let tri = face.triangle.vertex;
+
+         for index in 0..3 {
+            let (v0, v1, v2) = (tri[index], tri[(index+1)%3], tri[(index+2)%3]);
+
+            sphere += Triangle::new(v0,
+                                    ((v0+v1)/2.0).normalize(),
+                                    ((v0+v2)/2.0).normalize());
+         }
+
+         let (v0, v1, v2) = (tri[0], tri[1], tri[2]);
+         sphere += Triangle::new(((v0+v1)/2.0).normalize(),
+                                 ((v1+v2)/2.0).normalize(),
+                                 ((v0+v2)/2.0).normalize());
+      }
+      
+      sphere
    }
 
    
-   fn cube(size: Length, center: Point) -> Object {
+   fn sphere(radius: Length) -> Object {
+      let mut obj = Object::icosahedron(1.0);
+      // obj = Object::spherify(obj);
+      for _ in 1..=4 {
+         obj = Object::spherify(obj);
+      }
+      obj
+   }
+
+   
+   fn cube(size: Length) -> Object {
       Object::rectangular_prism(size, size, size)
    }
 
@@ -313,17 +396,17 @@ impl Object {
       let vy = Point{x: 0.0, y: depth, z: 0.0};
       let vz = Point{x: 0.0, y: 0.0, z: height};
 
-      let bottom = Shape::squarish(vx, vy);
+      let bottom = Shape::squarish(vy, vx);
       obj += &bottom;
-      obj += bottom + vz;
+      obj += (bottom + vz).inverted();
 
-      let side = Shape::squarish(vy, vz);
+      let side = Shape::squarish(vz, vy);
       obj += &side;
-      obj += side + vx;
+      obj += (side + vx).inverted();
 
       let front = Shape::squarish(vx, vz);
       obj += &front;
-      obj += front + vy;
+      obj += (front + vy).inverted();
       
       obj
    }
@@ -331,18 +414,38 @@ impl Object {
    
    fn cylinder(height: Length, radius: Length) -> Object {
       let mut obj = Shape::circle(radius);
-      // obj.extrude(height);
+      obj.extrude(height);
       obj
    }
 
    
-   fn polyhedron(points: Vec<Point>) {}
+   fn polyhedron(points: Vec<Point>) {
+
+   }
 
 
    //----------------------------------------------------------------------------
    // Transformations of Objects
    //----------------------------------------------------------------------------
 
+   fn extrude(&mut self, height: Length) {
+      
+      let offset: Vector = Vector { x: 0.0,
+                                    y: 0.0,
+                                    z: height };
+
+      for face in &mut self.faces {
+         face.invert();
+      }
+      
+      for face in &mut self.faces.clone() {
+         face.invert();
+         // let mut new_face: Face = face;
+         face.triangle += offset;
+         self.faces.push(*face);
+      }
+   }
+   
    fn translate(&mut self, by: &Vector) {
       *self += *by;
    }
@@ -374,12 +477,12 @@ impl<'a> AddAssign<&'a Object> for Object {
    }
 }
 
+
 impl AddAssign<Object> for Object {
    fn add_assign(&mut self, other: Object) {
       self.faces.extend(other.faces);
    }
 }
-
 
 
 impl AddAssign<Triangle> for Object {
@@ -394,7 +497,6 @@ impl AddAssign<Triangle> for Object {
 }
 
 
-
 impl Add<Vector> for Object {
    type Output = Object;
 
@@ -406,6 +508,7 @@ impl Add<Vector> for Object {
       obj
    }
 }
+
 
 impl AddAssign<Vector> for Object {
    fn add_assign(&mut self, vector: Vector) {
@@ -473,6 +576,7 @@ impl Shape {
                       Vector{x: 0.0, y: height, z: 0.0})
    }
 
+   
    fn polygon(vertices: Vec<Point>) -> Shape  {
       let mut shape = Shape::new();
       let p1 = vertices[0];
@@ -483,221 +587,7 @@ impl Shape {
       
       shape
    }
-   
-   fn icosahedron(radius: Length) -> Object {
-      // From Wikipedia:
-      //
-      // The locations of the vertices of a regular icosahedron can be
-      // described using spherical coordinates, for instance as
-      // latitude and longitude. If two vertices are taken to be at
-      // the north and south poles (latitude ±90°), then the other ten
-      // vertices are at latitude ±arctan(1/2) ≈ ±26.57°. These ten
-      // vertices are at evenly spaced longitudes (36° apart),
-      // alternating between north and south latitudes.
 
-      let obj = Object::new();
-      use Object::umbrella;
-      
-      let lat = 0.5_f32.atan();
-      let theta = PI/5.0;
-
-      obj += umbrella(Point{x: 0.0, y: 0.0, z: 1.0},
-                      (0..5).map(|i| Point{x: (theta*i as f32).cos(),
-                                           y: (theta*i as f32).sin(),
-                                           z: lat}).collect::Vec<Point>());
-      
-
-      obj
-   }
-   
-   fn sphere(radius: Length) {
-
-   }
-
-   
-   fn cube(size: Length, center: Point) -> Object {
-      Object::rectangular_prism(size, size, size)
-   }
-
-   
-   fn rectangular_prism(width: Length, depth: Length, height: Length) -> Object
-   {
-      let mut obj = Object::new();
-      
-      let vx = Point{x: width, y: 0.0, z: 0.0};
-      let vy = Point{x: 0.0, y: depth, z: 0.0};
-      let vz = Point{x: 0.0, y: 0.0, z: height};
-
-      let bottom = Shape::squarish(vx, vy);
-      obj += &bottom;
-      obj += bottom + vz;
-
-      let side = Shape::squarish(vy, vz);
-      obj += &side;
-      obj += side + vx;
-
-      let front = Shape::squarish(vx, vz);
-      obj += &front;
-      obj += front + vy;
-      
-      obj
-   }
-
-   
-   fn cylinder(height: Length, radius: Length) -> Object {
-      let mut obj = Shape::circle(radius);
-      // obj.extrude(height);
-      obj
-   }
-
-   
-   fn polyhedron(points: Vec<Point>) {}
-
-
-   //----------------------------------------------------------------------------
-   // Transformations of Objects
-   //----------------------------------------------------------------------------
-
-   fn translate(&mut self, by: &Vector) {
-      *self += *by;
-   }
-
-   
-   fn rotate(self, by: Vector) {}
-   
-   fn scale(&mut self, factor: f32) {
-      *self *= factor;
-   }
-
-
-   fn resize(&self, size: Point) {}
-   fn mirror(&mut self, around: Point) {}
-   // fn multmatrix() {}
-   fn color_by_name(&self, colour_name: String, alpha: f32) {}
-   fn color(&self, r: f32, g: f32, b: f32, a: f32) {}
-   fn offset(&self, r: Length) {}
-   fn hull(&self, other: &Object) {}
-   fn minkowski(&self, other: &Object) {}  
-
-}
-
-
-
-impl<'a> AddAssign<&'a Object> for Object {
-   fn add_assign(&mut self, other: &'a Object) {
-      self.faces.extend(&other.faces);
-   }
-}
-
-impl AddAssign<Object> for Object {
-   fn add_assign(&mut self, other: Object) {
-      self.faces.extend(other.faces);
-   }
-}
-
-
-
-impl AddAssign<Triangle> for Object {
-   fn add_assign(&mut self, triangle: Triangle) {
-      let face = Face {
-         normal: Vector {x: 0.0, y: 0.0, z: 1.0},
-         triangle: triangle,
-         colour: Colour {r: 0, g: 0, b: 0, alpha: 0}
-      };
-      self.faces.push(face);
-   }
-}
-
-
-
-impl Add<Vector> for Object {
-   type Output = Object;
-
-   fn add(self, vector: Vector) -> Object {
-      let mut obj = self.clone();
-      for face in &mut obj.faces {
-         face.triangle += vector;
-      }
-      obj
-   }
-}
-
-impl AddAssign<Vector> for Object {
-   fn add_assign(&mut self, vector: Vector) {
-      for face in &mut self.faces {
-         face.triangle += vector;
-      }  
-   }
-}
-
-impl MulAssign<Length> for Object {
-   fn mul_assign(&mut self, scale: Length) {
-      for face in &mut self.faces {
-         face.triangle *= scale;
-      }  
-   }
-}
-
-
-
-
-//------------------------------------------------------------------------------
-// 2D Shapes
-//------------------------------------------------------------------------------
-
-
-type Shape = Object;
-
-
-impl Shape {
-
-   fn new() -> Shape {
-      Shape{ faces: Vec::new() }
-   }
-
-   
-   fn circle(r: Length) -> Shape {
-      let angle = 2.0*PI/(FRAGMENTS as f32);
-      let mut points = Vec::new();
-      
-      for index in 0..FRAGMENTS {
-         let theta = index as f32 * angle;
-         points.push(Point::new(r*theta.cos(), r*theta.sin(), 0.0));
-      }
-
-      Shape::polygon(points)
-   }
-
-   
-   fn squarish(v1: Vector, v2: Vector) -> Shape {
-      let points = vec!{ORIGIN, v1, v1+v2, v2};
-      
-      Shape::polygon(points)
-   }
-
-   
-   fn square(l: Length) -> Shape
-   {
-      Shape::rectangle(l, l)
-   }
-
-
-   fn rectangle(width: Length, height: Length) -> Shape
-   {
-      Shape::squarish(Vector{x: width, y: 0.0, z: 0.0},
-                      Vector{x: 0.0, y: height, z: 0.0})
-   }
-
-   fn polygon(vertices: Vec<Point>) -> Shape {
-      let mut shape = Shape::new();
-      let p1 = vertices[0];
-      
-      for (p2, p3) in zip(&vertices[1..], &vertices[2..]) {
-         shape += Triangle::new(p1, *p2, *p3);
-      }
-      
-      shape
-   }
 
    fn text(text: String) {
    }
@@ -721,10 +611,6 @@ impl fmt::Display for Shape
 
 
 
-
-
-
-
 //==============================================================================
 // STL Files
 //==============================================================================
@@ -736,6 +622,7 @@ fn write_point(mut file: &File, point: &Point) -> Result<()>
    file.write_f32::<LittleEndian>(point.z)?;
    Ok(())
 }
+
 
 fn write_stl(filename: &str, obj: &Object)  -> std::io::Result<()>
 {
@@ -757,10 +644,12 @@ fn write_stl(filename: &str, obj: &Object)  -> std::io::Result<()>
    Ok(())
 }
 
+
 fn read_stl(filename: String) -> Option<Object>
 {
    None
 }
+
 
 fn write_text_stl(filename: &str, obj: &Object) -> std::io::Result<()>
 {
@@ -783,6 +672,7 @@ fn write_text_stl(filename: &str, obj: &Object) -> std::io::Result<()>
    Ok(())
 }
 
+
 fn read_text_stl(filename: String) -> Option<Object>
 {
    None
@@ -798,13 +688,14 @@ fn main() {
    println!("Rust CAD v.0.1");
    println!("==============");
  
+   /*
    let rectangle = Shape::rectangle(10.0, 20.0);
    println!("{}", rectangle);
    write_text_stl("test.stl", &rectangle);
-
+   */
    
-   let circle = Object::icosahedron(10.0);
-   println!("{}", circle);
-   write_stl("test_bin.stl", &circle);
+   let object = Object::cylinder(10.0, 10.0);
+   // println!("{}", circle);
+   write_stl("test_bin.stl", &object);
 
 }
